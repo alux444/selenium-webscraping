8097877ef09d06f6888b37e43ead2e250a011df7
alux444 <aluux999@gmail.com>
2023-09-12 20:27:22 +1200

    test update

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,5 @@
 const { By, Key, Builder } = require("selenium-webdriver");
+const { until } = require("selenium-webdriver");
 require("chromedriver");
 
 const testCase = async () => {
@@ -6,7 +7,46 @@ const testCase = async () => {
 
     await driver.get("https://alux444.github.io/tracktrekker/");
 
+    if ((await driver.getTitle()) === "TrackTrekker") {
+        console.log("Correct Page Name.");
+    } else {
+        console.log("Incorrect Page Name.");
+    }
+
+    //click the get started button
     await driver.findElement(By.className("button1")).click();
+
+    await driver.sleep(5000);
+
+    try {
+        // Locate the input element by its ID
+        const inputElement = await driver.findElement(By.id("songSearchBar"));
+
+        const placeholder = await inputElement.getAttribute("placeholder");
+
+        if (placeholder === "Search for Song") {
+            console.log("Correct prompt page upon loading");
+        }
+
+        await inputElement.sendKeys("Test Query");
+    } catch (error) {
+        console.error("Couldn't find songSearchBar:", error);
+    }
+
+    //switch to artists
+    try {
+        await driver.findElement(By.xpath("//span[text()='Artists']")).click();
+
+        const inputElement = await driver.findElement(By.id("artistSearchBar"));
+
+        const placeholder = await inputElement.getAttribute("placeholder");
+
+        if (placeholder === "Search for Artist") {
+            console.log("Correct prompt page on artists page switch");
+        }
+    } catch (error) {
+        console.log("Error with switching to artists:", error);
+    }
 
     setInterval(function () {
         driver.quit();
