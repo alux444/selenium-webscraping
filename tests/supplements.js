1f37886b884309ea29d9d20ebddea130b57f0e56
alux444 <aluux999@gmail.com>
2023-09-13 20:42:05 +1200

    added xplosiv scrape

--- tests/supplements.js
+++ tests/supplements.js
@@ -1,4 +1,4 @@
-const { By, Key, Builder } = require("selenium-webdriver");
+const { By, Key, Builder, until } = require("selenium-webdriver");
 require("chromedriver");
 
 const scrape = async () => {
@@ -7,7 +7,7 @@ const scrape = async () => {
     //nz muscle clearance
     await driver.get("https://www.nzmuscle.co.nz/clearance");
 
-    driver.sleep(3000);
+    driver.sleep(5000);
 
     itemElements = await driver.findElements(By.className("product-item-info"));
 
@@ -41,7 +41,7 @@ const scrape = async () => {
         "https://www.sprintfit.co.nz/products/clearance?pgNmbr=2&pgSize=25&isScrollChunk=true&chunkNumber=2"
     );
 
-    driver.sleep(3000);
+    driver.sleep(5000);
 
     const products = await driver.findElements(By.className("product"));
     console.log(
@@ -57,6 +57,85 @@ const scrape = async () => {
             .getText();
         const prices = await priceString.split(" ");
         console.log(`${title}: ${prices[1]} from ${prices[0]}`);
+    }
+
+    //asn
+    await driver.get("https://asnonline.co.nz/collections/supplement-specials");
+    driver.sleep(5000);
+
+    const asnItems = await driver.findElements(By.className("product-wrap"));
+
+    console.log(`Searching ASN specials - found ${asnItems.length} items`);
+
+    for (let i = 0; i < asnItems.length; i++) {
+        const title = await asnItems[i]
+            .findElement(By.className("product-thumbnail__title"))
+            .getText();
+        const price = await asnItems[i]
+            .findElement(By.className("product-thumbnail__price"))
+            .getText();
+        const prices = await price.split(" ");
+
+        console.log(`${title}: ${prices[0]} from ${prices[1]}`);
+    }
+
+    //xplosive
+    await driver.get("https://xplosiv.nz/clearance.html");
+
+    driver.sleep(5000);
+
+    let notFinished = true;
+    while (notFinished) {
+        try {
+            await driver.executeScript(
+                "window.scrollTo(0, document.body.scrollHeight);"
+            );
+
+            const timeout = 2000;
+            const button = await driver.wait(
+                until.elementLocated(By.className("amscroll-load-button")),
+                timeout
+            );
+
+            await button.click();
+        } catch (error) {
+            notFinished = false;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+    }
+
+    const xplosivItems = await driver.findElements(
+        By.className("product-item")
+    );
+
+    console.log(
+        `Searching Xplosiv clearance - found ${xplosivItems.length} items`
+    );
+
+    for (let i = 0; i < xplosivItems.length; i++) {
+        const title = await xplosivItems[i]
+            .findElement(By.className("product-item-name"))
+            .getText();
+        let price = "";
+
+        try {
+            const savedElement = await xplosivItems[i].findElement(
+                By.className("price")
+            );
+            price = await savedElement.getText();
+        } catch (error) {}
+
+        let saved = ""; // Default value
+
+        try {
+            const savedElement = await xplosivItems[i].findElement(
+                By.className("priceSavings")
+            );
+            saved = await savedElement.getText();
+            console.log(`${title}: ${price} ${saved}`);
+        } catch (error) {
+            console.log(`${title}: ${price}`);
+        }
     }
 
     setInterval(function () {
